3b434401c0ca1e3ec199573f9d175167c652dc36
dudesahn <dudesahn@gmail.com>
2022-06-12 20:52:52 -0400

    feat: update oracle to read from borrowBalanceStored
    read from borrowBalanceStored per cToken and aggregate those values

--- contracts/screamBadDebtOracle.sol
+++ contracts/screamBadDebtOracle.sol
@@ -4,12 +4,18 @@ pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/utils/Address.sol";
 
+interface IScream {
+ function borrowBalanceStored(address account) external view returns (uint);
+ }
+
+
 contract ScreamBadDebtOracle {
     using Address for address;
     
     /* ========== STATE VARIABLES ========== */
     address[] public badDebtAccounts; //@notice These accounts hold borrows with no supplied assets
     address public owner; //@notice The address of the owner, i.e. the Timelock contract, which can update parameters directly
+    IScream public constant unitroller = IScream(0x260E596DAbE3AFc463e75B6CC05d8c46aCAcFB09); //@notice Scream v1 unitroller
 
     /**
      * @notice Construct our bad debt oracle
@@ -19,6 +25,23 @@ contract ScreamBadDebtOracle {
     constructor(address[] memory _badDebtAccounts, address _owner) public {
         owner = _owner;
         badDebtAccounts = _badDebtAccounts;
+    }
+
+    /**
+     * @notice View for how much bad debt our accounts have in total (in USD)
+     * @return The total bad debt for all badDebtAccounts
+     */
+    function totalBadDebt(address scToken) external view returns (uint256) {
+        uint256 allBadDebt;
+        IScream scTokenContract = IScream(scToken);
+        for (uint256 i = 0; i < badDebtAccounts.length; i++) {
+            address badDebtHolder = badDebtAccounts[i];
+            uint256 borrowed = scTokenContract.borrowBalanceStored(badDebtHolder);
+            if (borrowed > 0) {
+                allBadDebt += borrowed;
+            }
+        }
+        return allBadDebt;
     }
 
     /**
